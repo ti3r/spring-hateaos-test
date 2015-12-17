fbf3e55facf88b3a32410bbda8f09aa2de00f08b
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2015-12-17 12:59:32 -0600

    Adding index.html and updating readme.

--- src/main/java/com/albiworks/test/rest/hateoas/SpringRestHateoasTestApplication.java
+++ src/main/java/com/albiworks/test/rest/hateoas/SpringRestHateoasTestApplication.java
@@ -3,8 +3,6 @@ package com.albiworks.test.rest.hateoas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mangofactory.swagger.plugin.EnableSwagger;
-
 @SpringBootApplication
 public class SpringRestHateoasTestApplication {
 
